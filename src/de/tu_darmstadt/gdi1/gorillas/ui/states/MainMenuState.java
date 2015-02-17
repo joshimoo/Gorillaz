855cbb21ff1d9769d817ef2d9643bc3422136c03
Gabriel Eckhardt <ge43nede@clientmaster.rbg.informatik.tu-darmstadt.de>
2015-02-17 13:23:26 +0100

    Buttons hinzugefügt

--- src/de/tu_darmstadt/gdi1/gorillas/ui/states/MainMenuState.java
+++ src/de/tu_darmstadt/gdi1/gorillas/ui/states/MainMenuState.java
@@ -3,21 +3,20 @@ package de.tu_darmstadt.gdi1.gorillas.ui.states;
 import de.matthiasmann.twl.Button;
 import de.matthiasmann.twl.slick.BasicTWLGameState;
 import de.matthiasmann.twl.slick.RootPane;
+import de.tu_darmstadt.gdi1.gorillas.main.Gorillas;
 import eea.engine.entity.StateBasedEntityManager;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class MainMenuState extends BasicTWLGameState {
 
     private int stateID;
     private StateBasedEntityManager entityManager;
-
     private Image background;
-
-    private Button newGameButton;
+    private Button btnNewGame;
+    private Button btnExit;
+    private Button btnMute;
+    private RootPane rp;
 
     public MainMenuState(int sid) {
         stateID = sid;
@@ -27,18 +26,44 @@ public class MainMenuState extends BasicTWLGameState {
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         background = new Image("assets/menu/background.png");
+
+        rp = super.createRootPane();
+
+        btnNewGame = new Button("New Game");
+        btnNewGame.addCallback(new Runnable() {
+            public void run() {
+                game.enterState(Gorillas.GAMESETUPSTATE);
+            }
+        });
+
+        btnExit = new Button("Exit Game");
+        btnExit.addCallback(new Runnable() {
+            public void run() {
+                /// FIXME: ist system.exit ok?
+                System.exit(0);
+            }
+        });
+
+        btnMute = new Button("Mute");
+        btnMute.addCallback(new Runnable() {
+            public void run() {
+                // TODO: Mute me :)
+            }
+        });
+
+        rp.add(btnNewGame);
+        rp.add(btnExit);
+        rp.add(btnMute);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-
         entityManager.updateEntities(container, game, delta);
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-
-        g.drawImage(background, 0, 0);
+        g.drawImage(background, -10, -20);
         entityManager.renderEntities(container, game, g);
     }
 
@@ -49,27 +74,22 @@ public class MainMenuState extends BasicTWLGameState {
 
     @Override
     protected RootPane createRootPane() {
-
-        RootPane rp = super.createRootPane();
-
-        newGameButton = new Button();
-        newGameButton.addCallback(new Runnable() {
-            public void run() {
-                // TODO: Enter next state
-            }
-        });
-
-        rp.add(newGameButton);
         return rp;
     }
 
     @Override
     protected void layoutRootPane() {
-
         int paneHeight = this.getRootPane().getHeight();
         int paneWidth = this.getRootPane().getWidth();
 
-        newGameButton.adjustSize();
-        newGameButton.setPosition(paneWidth / 2 - newGameButton.getWidth() / 2, paneHeight / 2 - newGameButton.getHeight() / 2);
+        btnNewGame.setSize(128, 32);
+        btnNewGame.setPosition(20, 20);
+
+        btnExit.setSize(128, 32);
+        btnExit.setPosition(20, 60);
+
+        btnMute.setSize(64, 64);
+        btnMute.setPosition(0, paneHeight - btnMute.getHeight());
     }
+
 }
