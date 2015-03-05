346ec7e6d89c0b4d95c7e6e7b17746a356aa3390
Tamara Liebich <tl40misa@rbg.informatik.tu-darmstadt.de>
2015-03-05 18:20:03 +0100

    Wind hinzugefuegt

---
+++ src/de/tu_darmstadt/gdi1/gorillas/entities/Cloud.java
@@ -0,0 +1,39 @@
+package de.tu_darmstadt.gdi1.gorillas.entities;
+
+import de.tu_darmstadt.gdi1.gorillas.assets.Assets;
+import de.tu_darmstadt.gdi1.gorillas.main.Gorillas;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+
+public class Cloud extends Entity {
+    private Image img;
+    private int w;
+    public static final float WSCALE = 0.6f;
+    private float t;
+
+    public Cloud(float x, float y, int w){
+        img = Assets.loadUniqueImage(Assets.Images.CLOUD);
+        this.x = x;
+        this.y = y;
+
+        this.w = w;
+        t = 0;
+
+    }
+    @Override
+    public void render(Graphics graph) {
+        graph.drawImage(img,x,y);
+    }
+
+    @Override
+    public void update(int delta) {
+        x += (WSCALE/2) * w;
+        if(x < -img.getWidth()) x = Gorillas.FRAME_WIDTH;
+        else if(x > Gorillas.FRAME_WIDTH) x = -img.getWidth();
+    }
+
+    @Override
+    public boolean isCollidding(Banana b) {
+        return false;
+    }
+}
